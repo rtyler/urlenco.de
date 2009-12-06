a40e0fb6d36feebe73acca7ea375a70cae2057cc
R. Tyler Ballance <tyler@monkeypox.org>
2009-12-06 00:34:35 -0800

    Minor cleanups, add actions for developers and stats views

--- Urlencode/controllers/home.py
+++ Urlencode/controllers/home.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from Urlencode import controllers
-from Urlencode import views
 
 class home(controllers.BaseController):
     content_type = 'text/html'
@@ -14,4 +13,10 @@ class home(controllers.BaseController):
     def about(self, **kwargs):
         return self.render('about')
 
+    @controllers.action(paths=('/Developer.aspx',))
+    def developers(self, **kwargs):
+        return 'Developers'
 
+    @controllers.action(paths=('/Stats.aspx',))
+    def stats(self, **kwargs):
+        return 'Stats'
--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 
 import sys
+
 from eventlet import api
 from eventlet import wsgi
 
 import Urlencode
 
 def main():
-    wsgi.server(api.tcp_listener(('', 8080)), Urlencode.app)
-    return 0
+    try:
+        wsgi.server(api.tcp_listener(('', 8080)), Urlencode.app)
+    except KeyboardInterrupt:
+        print '>>> Exiting cleanly...'
+        return 0
+    finally:
+        return -1
 
 if __name__ == '__main__':
     sys.exit(main())
